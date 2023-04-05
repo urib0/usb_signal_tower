557d6deb95f84ef116616afc6803f76ff7cbdaab
uribo <ur1b0.dev@gmail.com>
2023-04-06 05:55:17 +0900

    シリアル出力を追加

--- usb_signal_tower.ino
+++ usb_signal_tower.ino
@@ -1,8 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 7
-
 #define NUMPIXELS 3
+#define STATUS_OK 0
+#define VERSION "1.00"
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
@@ -10,11 +11,16 @@ Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
+	Serial.begin(9600);
 	pixels.begin();
+	while (!Serial)
+		delay(1);
 }
 
 void loop()
 {
+	static unsigned char visible_state[NUMPIXELS] = {0};
+
 	pixels.clear();
 	pixels.show();
 	delay(DELAYVAL);
@@ -28,4 +34,12 @@ void loop()
 
 		delay(DELAYVAL);
 	}
+
+	// serial feedback output
+	Serial.print("leds=" + String(NUMPIXELS) + ";");
+	for (int i = 0; i < NUMPIXELS; i++)
+	{
+		Serial.print(String(i + 1) + "=" + String(visible_state[i]) + ";");
+	}
+	Serial.print(";status=" + String(STATUS_OK) + ";VERSIONS=" + String(VERSION) + "\n");
 }
