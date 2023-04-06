d98f133aa9f11d4bfba796cb85635fff0c183ca2
uribo <ur1b0.dev@gmail.com>
2023-04-06 22:03:44 +0900

    update

--- usb_signal_tower.ino
+++ usb_signal_tower.ino
@@ -6,13 +6,18 @@
 #define VERSION "1.00"
 
 static char sc_colors[NUMPIXELS][3] = {
-	{20, 0, 0},
-	{10, 10, 0},
-	{0, 10, 0}};
+	{50, 0, 0},
+	{60, 60, 0},
+	{0, 60, 0}};
+
+static char sc_colors_weak[NUMPIXELS][3] = {
+	{15, 0, 0},
+	{20, 20, 0},
+	{0, 20, 0}};
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 100
+#define DELAYVAL 1000
 
 void setup()
 {
@@ -31,20 +36,25 @@ void loop()
 	unsigned char uc_visible_tmp[NUMPIXELS] = {0};
 	unsigned char tmp = 0;
 	unsigned char recv_cnt = 0;
+	static unsigned int interval_cnt = 0;
 
 	// check input
-	while (Serial.available() > 0){
+	while (Serial.available() > 0)
+	{
 		tmp = Serial.read();
 		Serial.print(tmp);
-		if ((tmp == '0') || tmp == '1'){
+		if ((tmp == '0') || tmp == '1')
+		{
 			uc_visible_tmp[recv_cnt] = tmp - '0';
 			recv_cnt++;
 		}
-		else{
+		else
+		{
 			break;
 		}
 	}
-	if (recv_cnt == NUMPIXELS){
+	if (recv_cnt == NUMPIXELS)
+	{
 		for (int i = 0; i < NUMPIXELS; i++)
 		{
 			visible_state[i] = uc_visible_tmp[i];
@@ -54,21 +64,28 @@ void loop()
 	// neopixles output
 	for (int i = 0; i < NUMPIXELS; i++)
 	{
-		if ( visible_state[i] == 1 ){
+		if (visible_state[i] == 1)
+		{
 			pixels.setPixelColor(i, pixels.Color(sc_colors[i][0], sc_colors[i][1], sc_colors[i][2]));
 		}
-		else{
-			pixels.setPixelColor(i,pixels.Color(0,0,0));
+		else
+		{
+			pixels.setPixelColor(i, pixels.Color(sc_colors_weak[i][0], sc_colors_weak[i][1], sc_colors_weak[i][2]));
 		}
 	}
 	pixels.show();
 
-	// serial feedback output
-	Serial.print("leds=" + String(NUMPIXELS) + ";");
-	for (int i = 0; i < NUMPIXELS; i++)
+	if (interval_cnt >= DELAYVAL)
 	{
-		Serial.print(String(i + 1) + "=" + String(visible_state[i]) + ";");
+		interval_cnt = 0;
+		// serial feedback output
+		Serial.print("leds=" + String(NUMPIXELS) + ";");
+		for (int i = 0; i < NUMPIXELS; i++)
+		{
+			Serial.print(String(i + 1) + "=" + String(visible_state[i]) + ";");
+		}
+		Serial.print(";status=" + String(STATUS_OK) + ";VERSIONS=" + String(VERSION) + "\n");
 	}
-	Serial.print(";status=" + String(STATUS_OK) + ";VERSIONS=" + String(VERSION) + "\n");
-	delay(DELAYVAL);
+	interval_cnt++;
+	delay(1);
 }
